61d9ab791fd8a6153191eb6c5688448412f12c20
Nova <xplodnstar@live.com>
2019-04-22 11:36:27 -0700

    using true API call now

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,8 +2,8 @@ import store from '../store'
 import axios from 'axios'
 
 export function callProfile() {
-    axios.get('http://localhost:3001/users').then(resp => {
-        // axios.get('https://api.github.com/users/xplodnstar').then(resp => {
+    // axios.get('http://localhost:3001/users').then(resp => {
+    axios.get('https://api.github.com/users/xplodnstar').then(resp => {
         store.dispatch({
             type: 'CALL_PROFILE',
             users: resp.data
@@ -12,8 +12,8 @@ export function callProfile() {
 }
 
 export function callRepos() {
-    axios.get('http://localhost:3001/repos').then(resp => {
-        //axios.get('https://api.github.com/users/xplodnstar/repos').then(resp => {
+    // axios.get('http://localhost:3001/repos').then(resp => {
+    axios.get('https://api.github.com/users/xplodnstar/repos').then(resp => {
         store.dispatch({
             type: 'CALL_REPOS',
             repos: resp.data
